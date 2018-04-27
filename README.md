6c20734f356e9b55c5198100511626f81c1954d7
Greg Haygood <ghaygood@gmail.com>
2018-04-27 10:56:08 -0400

    updated for new authentication method

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # Yelp Fusion Proxy
 
-This is an example proxy script for working with the 
+This is an example proxy script for working with the API-key version of the 
 [Yelp Fusion API](https://www.yelp.com/developers/documentation/v3/get_started) from a browser. _It is meant 
 for instructional purposes only, and should not be used in a production environment with additional security measures._
 
 
 ## Usage
 
-First, copy config-sample.php to config.php, and update the parameters with your own app keys.
+First, [register your app](https://www.yelp.com/developers/v3/manage_app) to get an API KEY.
 
-Then to call an API, pass a parameter **_ep** that contains the desired endpoint within the API, 
+Then to call any API, pass a parameter **_ep** that contains the desired endpoint within the API, 
 beginning with a forward slash:
 
 ```$javascript
-jQuery.getJSON("./yelp.php?_ep=/businesses/search&term=Taco+Mac");
+fetch("./yelp.php?_ep=/businesses/search&term=Taco+Mac");
 ```
 
 Note that the endpoint will be prepended with *https://api.yelp.com/v3* (no trailing slash).
--- yelp.php
+++ yelp.php
@@ -1,43 +1,28 @@
 <?php
 
-require_once( 'config.php' );
-
-define( 'API_BASE', 'https://api.yelp.com' );
-
-// OAuth credential placeholders that must be filled in by users.
-// You can find them on
-// https://www.yelp.com/developers/v3/manage_app
-define( 'CLIENT_ID', API_KEY );
-define( 'CLIENT_SECRET', API_SECRET );
-
-// Complain if credentials haven't been filled out.
-//assert( CLIENT_ID, "Please supply your client_id." );
-//assert( CLIENT_SECRET, "Please supply your client_secret." );
-
-// API constants, you shouldn't have to change these.
-$TOKEN_PATH = "/oauth2/token";
-$GRANT_TYPE = "client_credentials";
+define('API_BASE', 'https://api.yelp.com');
 
 session_start();
 
 $endpoint = @$_REQUEST['_ep'];
-header( 'Content-Type: application/json' );
+header('Content-Type: application/json');
 
-if ( !empty( $endpoint ) ) {
-//	$args = http_build_query( $_REQUEST );
-
-	$bearer_token = obtain_bearer_token();
-
-	echo request( $bearer_token, API_BASE . '/v3', $endpoint, $_GET );
-
+if (!empty($endpoint)) {
+    $headers      = apache_request_headers();
+    $bearer_token = $headers['Authorization'];
+    if (empty($bearer_token) || strlen($bearer_token) < 8) {
+        error('No API key provided');
+    }
+    echo request($bearer_token, API_BASE . '/v3', $endpoint, $_REQUEST);
 } else {
-	error( 'No endpoint requested' );
+    error('No endpoint requested');
 }
 
-function error( $message ) {
-	http_response_code( 400 );
-	echo json_encode( array( 'error' => $message ) );
-	die;
+function error($message)
+{
+    http_response_code(400);
+    echo json_encode(array('error' => $message));
+    die;
 }
 
 /**
@@ -55,66 +40,6 @@ function error( $message ) {
  * `php sample.php --term="dinner" --location="San Francisco, CA"`
  */
 
-
-/**
- * Given a bearer token, send a GET request to the API.
- *
- * @return   OAuth bearer token, obtained using client_id and client_secret.
- */
-
-function obtain_bearer_token() {
-	try {
-		# Using the built-in cURL library for easiest installation.
-		# Extension library HttpRequest would also work here.
-		$curl = curl_init();
-		if ( false === $curl ) {
-			throw new Exception( 'Failed to initialize' );
-		}
-
-		$postfields = "client_id=" . CLIENT_ID .
-		              "&client_secret=" . CLIENT_SECRET .
-		              "&grant_type=" . $GLOBALS['GRANT_TYPE'];
-
-		curl_setopt_array( $curl, array(
-			CURLOPT_URL            => API_BASE . $GLOBALS['TOKEN_PATH'],
-			CURLOPT_RETURNTRANSFER => true,  // Capture response.
-			CURLOPT_ENCODING       => "",  // Accept gzip/deflate/whatever.
-			CURLOPT_MAXREDIRS      => 10,
-			CURLOPT_TIMEOUT        => 30,
-			CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST  => "POST",
-			CURLOPT_POSTFIELDS     => $postfields,
-			CURLOPT_HTTPHEADER     => array(
-				"cache-control: no-cache",
-				"content-type: application/x-www-form-urlencoded",
-			),
-		) );
-
-		$response = curl_exec( $curl );
-
-		if ( false === $response ) {
-			throw new Exception( curl_error( $curl ), curl_errno( $curl ) );
-		}
-		$http_status = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
-		if ( 200 != $http_status ) {
-			throw new Exception( $response, $http_status );
-		}
-
-		curl_close( $curl );
-	} catch ( Exception $e ) {
-		trigger_error( sprintf(
-			'Curl failed with error #%d: %s',
-			$e->getCode(), $e->getMessage() ),
-			E_USER_ERROR );
-	}
-
-	$body         = json_decode( $response );
-	$bearer_token = $body->access_token;
-
-	return $bearer_token;
-}
-
-
 /**
  * Makes a request to the Yelp API and returns the response
  *
@@ -125,47 +50,48 @@ function obtain_bearer_token() {
  *
  * @return   The JSON response from the request
  */
-function request( $bearer_token, $host, $path, $url_params = array() ) {
-	// Send Yelp API Call
-	try {
-		$curl = curl_init();
-		if ( false === $curl ) {
-			throw new Exception( 'Failed to initialize' );
-		}
+function request($bearer_token, $host, $path, $url_params = array())
+{
+    // Send Yelp API Call
+    try {
+        $curl = curl_init();
+        if (false === $curl) {
+            throw new Exception('Failed to initialize');
+        }
 
-		$url = $host . $path . "?" . http_build_query( $url_params );
-		curl_setopt_array( $curl, array(
-			CURLOPT_URL            => $url,
-			CURLOPT_RETURNTRANSFER => true,  // Capture response.
-			CURLOPT_ENCODING       => "",  // Accept gzip/deflate/whatever.
-			CURLOPT_MAXREDIRS      => 10,
-			CURLOPT_TIMEOUT        => 30,
-			CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST  => "GET",
-			CURLOPT_HTTPHEADER     => array(
-				"authorization: Bearer " . $bearer_token,
-				"cache-control: no-cache",
-			),
-		) );
+        $url = $host . $path . "?" . http_build_query($url_params);
+        curl_setopt_array($curl, array(
+            CURLOPT_URL            => $url,
+            CURLOPT_RETURNTRANSFER => true,  // Capture response.
+            CURLOPT_ENCODING       => "",  // Accept gzip/deflate/whatever.
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => "GET",
+            CURLOPT_HTTPHEADER     => array(
+                "authorization:" . $bearer_token,
+                "cache-control: no-cache",
+            ),
+        ));
 
-		$response = curl_exec( $curl );
+        $response = curl_exec($curl);
 
-		if ( false === $response ) {
-			throw new Exception( curl_error( $curl ), curl_errno( $curl ) );
-		}
-		$http_status = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
-		if ( 200 != $http_status ) {
-			throw new Exception( $response, $http_status );
-		}
+        if (false === $response) {
+            throw new Exception(curl_error($curl), curl_errno($curl));
+        }
+        $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if (200 != $http_status) {
+            throw new Exception($response, $http_status);
+        }
 
-		curl_close( $curl );
-	} catch ( Exception $e ) {
-		trigger_error( sprintf(
-			'Curl failed with error #%d: %s',
-			$e->getCode(), $e->getMessage() ),
-			E_USER_ERROR );
-	}
+        curl_close($curl);
+    } catch (Exception $e) {
+        trigger_error(sprintf(
+            'Curl failed with error #%d: %s',
+            $e->getCode(), $e->getMessage()),
+            E_USER_ERROR);
+    }
 
-	return $response;
+    return $response;
 }
 
